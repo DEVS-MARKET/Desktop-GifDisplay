8c805925db158e5563acaa67e1a7cb4a92ccadab
0WhiteDev <0whitedev@gmail.com>
2024-08-06 01:46:45 +0200

    Version 1.4
    - Fixed custom songs player

--- src/main/java/uk/whitedev/desktop/displays/functions/MusicFunc.java
+++ src/main/java/uk/whitedev/desktop/displays/functions/MusicFunc.java
@@ -4,11 +4,16 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
+import java.io.File;
+
 public class MusicFunc {
     private MediaPlayer mediaPlayer;
 
     public void playMusic(String musicPath, String defaultMusic) {
-        String path = musicPath.isEmpty() ? getClass().getResource(defaultMusic.equals("GoBang") ? "/assets/music/GoBang Music.wav" : "/assets/music/Default Song.wav").toString() : musicPath;
+        String path = musicPath.isEmpty()
+                ? getClass().getResource(defaultMusic.equals("GoBang") ? "/assets/music/GoBang Music.wav" : "/assets/music/Default Song.wav").toString()
+                : new File(musicPath).toURI().toString();
+
         Media media = new Media(path);
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
